49a495adee286a275a6b2b6d6c1f0ee271901473
Doug <doug.hamil@gmail.com>
2015-04-01 16:51:28 -0600

    Replaced ground textures. Added fist weapon that applies force to hit object.

---
+++ Assets/Scripts/Weapons/Fist.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fist : MonoBehaviour {
+
+	public float Force = 10f;
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.attachedRigidbody != null)
+		{
+			Vector3 dir = ( other.transform.position - this.transform.position).normalized;
+			Vector2 dir2D = new Vector2(dir.x, dir.y);
+			other.attachedRigidbody.AddForce(dir2D * Force);
+		}
+	}
+}
