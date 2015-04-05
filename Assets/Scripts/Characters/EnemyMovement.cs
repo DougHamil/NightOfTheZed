18cfc9534aec0123f43150b7e153b0321402220a
wmatson <wmatson@users.noreply.github.com>
2015-04-04 19:32:26 -0600

    Very simple enemy movement
    Rotation is very odd at this point

---
+++ Assets/Scripts/Characters/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public GameObject target;
+	public float speed = 20;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var direction = (target.transform.position - this.transform.position).normalized;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (direction.x, direction.y) * speed;
+		this.transform.rotation = Quaternion.LookRotation (direction, Vector3.forward);
+	}
+}
