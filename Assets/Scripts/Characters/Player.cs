b85afcafeb26c9f5a6defe7433cc4897ad85eb66
Doug Hamilton <doug.hamil@gmail.com>
2015-04-06 18:19:58 -0600

    Added shotgun weapon

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-	public ParticleSystem tempFiringSystem;
 	public Transform TorsoTransform;
 	public Transform LegsTransform;
+	public Transform TorsoSkeletonRoot;
 	public float MoveSpeed = 10.0f;
 	public bool rightStickAim = false;
 
 	private Animator torsoAnimator;
 	private Animator legsAnimator;
 	private Rigidbody2D rigidBody;
+	private Shotgun activeWeapon;
 
 	private delegate Quaternion getRotationDelegate();
-
 	private getRotationDelegate getRotation;
 	
 	void Start () {
@@ -42,15 +42,36 @@ public class Player : MonoBehaviour
 		// Update Aim
 		TorsoTransform.rotation = getRotation();
 		TorsoTransform.eulerAngles = new Vector3(0, 0, TorsoTransform.eulerAngles.z);
+		if(activeWeapon != null)
+			activeWeapon.SetAimPoint(this.transform.forward * 500f);
 
 		// Update Animations
 		legsAnimator.SetBool("isWalking", isMoving);
 		torsoAnimator.SetBool("isWalking", isMoving);
 		torsoAnimator.SetBool("isFiring", Input.GetButton("Fire1"));
 		
-		if(Input.GetButton("Fire1") && tempFiringSystem != null)
+		if(Input.GetButtonDown("Fire1") && activeWeapon != null)
+			activeWeapon.BeginFire(this.transform.forward * 500f);
+		if(Input.GetButtonUp("Fire1") && activeWeapon != null)
+			activeWeapon.EndFire();
+	}
+
+	public void PickUpWeapon(Shotgun weapon)
+	{
+		this.activeWeapon = weapon;
+		this.torsoAnimator.SetBool("isHoldingWeapon", true);
+		if(weapon.AttachmentPoint != null)
 		{
-			tempFiringSystem.Play();
+			Vector3 offset = weapon.transform.position -weapon.AttachmentTransform.position;
+			Vector3 scale = weapon.transform.localScale;
+			Transform attachParent = this.TorsoSkeletonRoot.FindChild(weapon.AttachmentPoint);
+
+			weapon.transform.parent = attachParent;
+			weapon.transform.localPosition = offset;
+			weapon.transform.position = new Vector3(weapon.transform.position.x, weapon.transform.position.y, attachParent.position.z - 1.5f);
+			weapon.transform.rotation = attachParent.rotation;
+			weapon.transform.Rotate(new Vector3(0, 0, -90f));
+			weapon.transform.localScale = scale;
 		}
 	}
 
---
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shotgun : MonoBehaviour {
+
+	public string AttachmentPoint = "hand_r";
+	public int BulletsPerShot = 12;
+	public float FiringDelay = 1f;
+	public Transform Muzzle;
+	public Transform AttachmentTransform;
+
+	private bool isFiring = false;
+	private float shotTimer = 0f;
+	private Vector3 aimPoint = Vector3.zero;
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		Player player = other.gameObject.GetComponent<Player>();
+
+		if(player != null)
+		{
+			player.PickUpWeapon(this);
+			this.gameObject.GetComponent<Collider2D>().enabled = false;
+		}
+	}
+
+	public void BeginFire(Vector3 aimPoint){
+		isFiring = true;
+		this.aimPoint = aimPoint;
+	}
+
+	public void EndFire(){
+		isFiring = false;
+	}
+
+	public void SetAimPoint(Vector3 aimPoint) {
+		this.aimPoint = aimPoint;
+	}
+
+	private void fire() {
+		Vector2 sourcePosition = new Vector2(this.Muzzle.position.x, this.Muzzle.position.y);
+		Vector2 destPosition = new Vector2(this.aimPoint.x, this.aimPoint.y);
+		RaycastHit2D hit = Physics2D.Raycast(sourcePosition, (destPosition - sourcePosition).normalized);
+		if(hit.collider != null)
+		{
+			Debug.Log (hit.collider.gameObject);
+		}
+	}
+
+	void Update () {
+		if(shotTimer > 0f)
+			shotTimer -= Time.deltaTime;
+		if(isFiring && shotTimer <= 0f)
+		{
+			shotTimer = FiringDelay;
+			fire();
+		}
+	}
+}
