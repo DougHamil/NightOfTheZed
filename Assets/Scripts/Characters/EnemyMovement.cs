cb46c8e57dbf683d3f52fa258265bbf73065f519
Doug Hamilton <doug.hamil@gmail.com>
2015-04-06 18:53:31 -0600

    Fixed player animations. Wired zombie animations to movement.

--- Assets/Scripts/Characters/EnemyMovement.cs
+++ Assets/Scripts/Characters/EnemyMovement.cs
@@ -3,17 +3,39 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 
+	public float attackRange = 1f;
 	public GameObject target;
 	public float speed = 20;
-	// Use this for initialization
+	public Animator TorsoAnimator;
+
+	private Rigidbody2D rigidBody;
+
 	void Start () {
-	
+		this.rigidBody = this.GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		var direction = (target.transform.position - this.transform.position).normalized;
-		GetComponent<Rigidbody2D> ().velocity = new Vector2 (direction.x, direction.y) * speed;
-		this.transform.rotation = Quaternion.LookRotation (direction, Vector3.forward);
+		// Update Movement
+		var targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+		var direction = (targetPosition - this.transform.position);
+		float distance = direction.magnitude;
+
+		bool isMoving = distance > this.attackRange;
+		bool isAttacking = !isMoving;
+
+		if(isMoving)
+		{
+			direction.Normalize();
+			this.rigidBody.velocity = new Vector2 (direction.x, direction.y) * speed;
+		}
+
+		// Update Rotation
+		this.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2 (direction.y,direction.x)*Mathf.Rad2Deg - 90f, Vector3.forward);
+		this.transform.eulerAngles = new Vector3(0f, 0f, this.transform.eulerAngles.z);
+		this.rigidBody.rotation = this.transform.eulerAngles.z;
+
+		// Update animation
+		this.TorsoAnimator.SetBool("isWalking", isMoving);
+		this.TorsoAnimator.SetBool("isAttacking", isAttacking);
 	}
 }
